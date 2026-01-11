e23be0d59444f4dddfb595eb59fe28cc51289648
Ahmed Wagdy <ahmedwa122@gmail.com>
2026-01-11 17:46:17 +0200

    feat: add hero banner component and update register page layout
    - Implement new HeroBanner component with title display
    - Refactor register page to use HeroBanner and improve form layout
    - Update header styling and icon
    - Change global background color to #FEFEFE

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,4 +1,6 @@
+import HeroBanner from "@/components/heroBanner";
 import { Metadata } from "next";
+import RegisterForm from "./register-form";
 
 export const metadata: Metadata = {
   title: "Register",
@@ -6,10 +8,12 @@ export const metadata: Metadata = {
 
 export default function RegisterPage() {
   return (
-    <div className="w-full max-w-md">
-      <div className="rounded-lg shadow-md p-8">
-        <h3 className="text-[32px] font-semibold mb-2">Register</h3>
-        <p className="text-[#8A8A8A] mb-8">Create your account</p>
+    <div className="">
+      <HeroBanner title="Register" />
+      <div className="w-full max-w-md mt-8 ">
+        <div className="w-full flex justify-center rounded-lg shadow-md px-4 sm:px-6 lg:px-8 py-4">
+          <RegisterForm />
+        </div>
       </div>
     </div>
   );
--- components/header.tsx
+++ components/header.tsx
@@ -1,9 +1,9 @@
-import { MenuIcon } from "lucide-react";
+import { Menu } from "lucide-react";
 import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="mx-auto flex h-20 max-w-7xl items-center gap-8 px-4 sm:px-6 lg:px-8 shadow lg:shadow-2xl">
+    <header className="mx-auto flex h-20 items-center gap-8 px-4 sm:px-6 lg:px-8 shadow-md lg:shadow-lg">
       <Link className="block" href="/">
         <span className="sr-only">Home</span>
         <img className="size-16" src="/logo.svg" alt="logo" />
@@ -11,7 +11,7 @@ export default function Header() {
       <div className="flex flex-1 items-center justify-end md:justify-between">
         <button className="cursor-pointer hover:bg-slate-100 block text-lg rounded-lg font-bold md:hidden">
           <span className="sr-only">Toggle menu</span>
-          <MenuIcon className="size-10" />
+          <Menu strokeWidth={3} className="size-8" />
         </button>
       </div>
     </header>
