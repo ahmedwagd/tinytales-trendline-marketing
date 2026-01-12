5fef9464c64caf8b39cf025dcc4a2228c4366fa9
Ahmed Wagdy <ahmedwa122@gmail.com>
2026-01-12 14:45:05 +0200

    feat: add breadcrumbs component for navigation hierarchy

---
+++ components/breadcrumbs.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import { ChevronRight } from "lucide-react";
+import { ReactNode } from "react";
+
+export default function Breadcrumbs() {
+  return (
+    <nav className=" text-sm bg-[#ECECEC66] my-4 px-4 py-4 rounded-xl">
+      <div className="flex items-center">
+        <BreadcrumbLink href="/">Home</BreadcrumbLink>
+        <BreadcrumbLink href="/category">Our Category</BreadcrumbLink>
+        <span className="text-[#8A8A8A]">Product Details</span>
+      </div>
+    </nav>
+  );
+}
+
+function BreadcrumbLink({
+  href,
+  children,
+}: {
+  href: string;
+  children: ReactNode;
+}) {
+  return (
+    <>
+      <Link href={href} className="inline-block hover:text-black">
+        {children}
+      </Link>
+      <ChevronRight className="inline-block w-4 h-4 mx-2" />
+    </>
+  );
+}
